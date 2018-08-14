25c695bd87a92852efd0e667dac494e796665dfa
sendventures <38688816+sendventures@users.noreply.github.com>
2018-08-14 16:47:21 +0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -11,10 +11,42 @@
 	
 	
 	
-	<main>
+/*	<main>
 
         <p>store email</p>
         <form action="contactform.php" method="post">
+            <input type="text" name="name" placeholder="full name">
+            <input type="text" name="email" placeholder="your email">
+            <button type="submit" name="submit">Subscribe</button>
+        </form>
+
+    </main>
+	*/
+	
+	
+	
+	<?php
+      if(isset($_POST['submit']))
+      {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $file = fopen("text.txt","w+") or die("file not open"); //create and open text file
+        $s = $name.",".$email."\n";
+        fputs($file,$s) or die("Data is gay"); //write single line to file
+        fclose($file); //close file
+      }
+
+
+//echo file_put_contents("formdata.txt","Hello World. Testing!");
+
+      ?>
+
+
+
+    <main>
+
+        <p>store email</p>
+        <form action="#" method="POST">
             <input type="text" name="name" placeholder="full name">
             <input type="text" name="email" placeholder="your email">
             <button type="submit" name="submit">Subscribe</button>
